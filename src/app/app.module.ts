8e1e9a6bc370cf69303423c0d5f874fbd8e525e0
Bruno <brunojcastro95@gmail.com>
2022-10-13 12:09:19 -0500

    steps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { APP_ROUTING } from './app.routes';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { DatauserComponent } from './pages/datauser/datauser.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import{ MatTabsModule } from '@angular/material/tabs'
+import{ MatStepperModule } from '@angular/material/stepper'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DatauserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING,
+    BrowserAnimationsModule,
+    MatTabsModule,
+    MatStepperModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(
+              public router: Router 
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+}
